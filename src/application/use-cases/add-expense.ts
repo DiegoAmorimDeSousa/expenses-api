d37007d201bc2fa58e9c2b63eefd0b192582ccd4
DiegoAmorimDeSousa <diegoassousa@gmail.com>
2025-08-11 19:09:12 -0300

    first commit

--- src/application/use-cases/add-expense.ts
+++ src/application/use-cases/add-expense.ts
@@ -4,23 +4,24 @@ import { IGoogleSheetsGateway } from '../../domain/i-google-sheets-gateway';
 export interface AddExpenseParams {
     description: string;
     category: string;
-    value: number;
+    cost: number;
     date: string;
     user: string;
+    type: string;
 }
 
 export class AddExpenseUseCase {
     constructor(private readonly googleSheetsGateway: IGoogleSheetsGateway) {}
 
     async execute(params: AddExpenseParams): Promise<void> {
-        if (!params.description || !params.category || !params.value || params.value <= 0 || !params.date) {
+        if (!params.description || !params.category || !params.cost || params.cost <= 0 || !params.date) {
             throw new Error('Dados de gasto inválidos: descrição, categoria, valor ou data ausentes/inválidos.');
         }
 
         const expense: Expense = {
             description: params.description,
             category: params.category,
-            cost: params.value,
+            cost: params.cost,
             date: params.date,
             user: params.user,
             type: "Saída",
